00e861535f9f1c69a6673876333d437105a8b7c3
Matthew Reimer <matt.reimer@gmail.com>
2014-10-21 12:15:01 -0700

    returning a value

--- ModelCore/unit.cpp
+++ ModelCore/unit.cpp
@@ -9,12 +9,12 @@ Unit::Unit(const char * sName, int nID) : NamedObjectWithID(sName, nID)
 
 QString Unit::getAbbreviation()
 {
-
+    return s_abbreviation;
 }
 
 NamedObjectWithID Unit::getDimension()
 {
-
+    return * m_dimension;
 }
 
 }
